79c31fb61b5a8813d87ad9aa302f534a22278407
Tien Nguyen <tiennguyen2283@gmail.com>
2024-03-22 17:55:53 +0700

    add FileUpload to local database

---
+++ aspnet-core/src/MyCompanyName.AbpZeroTemplate.Application/FileUpload/ExamFileAppService.cs
@@ -0,0 +1,48 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.Domain.Repositories;
+using AutoMapper;
+using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Extensions;
+using Abp.Linq.Extensions;
+
+namespace MyCompanyName.AbpZeroTemplate.FileUpload
+{
+    public class ExamFileAppService : AbpZeroTemplateAppServiceBase, IExamFileAppService
+    {
+        private readonly IRepository<ExamFile> _examFileRepository;
+
+        public ExamFileAppService(IRepository<ExamFile> examFileRepository)
+        {
+            _examFileRepository = examFileRepository;
+        }
+
+        public ListResultDto<ExamFileListDto> GetPeople(GetExamFilesInput input)
+        {
+            var people = _examFileRepository
+                .GetAll()
+                .WhereIf(
+                    !input.Filter.IsNullOrEmpty(),
+                    p => p.Description.Contains(input.Filter) ||
+                         p.FilePath.Contains(input.Filter)
+                )
+                .OrderBy(p => p.Description)
+                .ThenBy(p => p.FilePath)
+                .ToList();
+
+            return new ListResultDto<ExamFileListDto>(ObjectMapper.Map<List<ExamFileListDto>>(people));
+        }
+
+        public async Task CreateExamFile(CreateExamFileInput input)
+        {
+            var examFile = ObjectMapper.Map<ExamFile>(input);
+            await _examFileRepository.InsertAsync(examFile);
+        }
+
+    }
+
+}
---
+++ aspnet-core/src/MyCompanyName.AbpZeroTemplate.Core.Shared/FileUpload/ExamFileConsts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCompanyName.AbpZeroTemplate.FileUpload
+{
+    public class ExamFileConsts
+    {
+        public const int MaxDescriptionLength = 255;
+        public const int MaxFilePathLength = 255;
+    }
+}
