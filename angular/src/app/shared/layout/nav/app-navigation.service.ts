cab84e5c85145f67363cb7d78a643bc872f4b657
TanNguyen <duytan17112003@gmail.com>
2024-03-26 22:23:57 +0700

    feat(file-upload-api): no special change

--- angular/src/app/shared/layout/nav/app-navigation.service.ts
+++ angular/src/app/shared/layout/nav/app-navigation.service.ts
@@ -20,6 +20,7 @@ export class AppNavigationService {
                 'flaticon-line-graph',
                 '/app/admin/hostDashboard'
             ),
+            new AppMenuItem('CreateExams', null, 'flaticon-folder', '/app/main/create-questions'),
             new AppMenuItem('Dashboard', 'Pages.Tenant.Dashboard', 'flaticon-line-graph', '/app/main/dashboard'),
             new AppMenuItem('Tenants', 'Pages.Tenants', 'flaticon-list-3', '/app/admin/tenants'),
             new AppMenuItem('Editions', 'Pages.Editions', 'flaticon-app', '/app/admin/editions'),
